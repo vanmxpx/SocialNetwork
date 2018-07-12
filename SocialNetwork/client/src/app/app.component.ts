dbc64fd3e81167894e6b8577666a66e4dc734df8
Nikita Tsyhankov <nikita1996.tsyhankov@gmail.com>
2018-07-12 11:38:51 +0300

    Angular-cli version updated

--- SocialNetwork/client/src/app/app.component.ts
+++ SocialNetwork/client/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
   constructor() {
     var test: string = "Debug is working!";
   }
