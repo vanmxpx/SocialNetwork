e149ff278275581b31abc8f208c805163a393ab6
Misha Krygin <Noderoid64@gmail.com>
2018-08-06 11:49:48 +0300

    feature add migration that sets the default gender value

---
+++ SocialNetwork/Migrations/20180806083907_MoveGenderToEnum.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialNetwork.Migrations
+{
+    public partial class MoveGenderToEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Gender",
+                table: "Userdata",
+                nullable: false,
+                defaultValue: (byte)2,
+                oldClrType: typeof(byte),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Gender",
+                table: "Userdata",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldDefaultValue: (byte)2);
+        }
+    }
+}
