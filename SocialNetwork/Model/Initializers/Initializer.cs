9af5201a414e187cfccb105f946652a0a1ed5f75
Misha Krygin <Noderoid64@gmail.com>
2018-08-06 11:19:49 +0300

    feature add migration (Replace type of gender in UserData)

--- SocialNetwork/Model/Initializers/Initializer.cs
+++ SocialNetwork/Model/Initializers/Initializer.cs
@@ -34,20 +34,23 @@ namespace SocialNetwork
 
         public async Task Seed()
         {
-         //   if (!SC.Authorizations.Any())
-         //       SC.Authorizations.RemoveRange(SC.Authorizations);
+            //   if (!SC.Authorizations.Any())
+            //       SC.Authorizations.RemoveRange(SC.Authorizations);
 
-         //   if (!SC.Followers.Any())
-         //       SC.Followers.RemoveRange(SC.Followers);
+            //   if (!SC.Followers.Any())
+            //       SC.Followers.RemoveRange(SC.Followers);
 
-          //  if (!SC.Posts.Any())
-         //       SC.Posts.RemoveRange(SC.Posts);
-
-          //  if (!SC.Userdata.Any())
-          //      SC.Userdata.RemoveRange(SC.Userdata);
-
+            //  if (!SC.Posts.Any())
+            //       SC.Posts.RemoveRange(SC.Posts);
             if (!SC.Profiles.Any())
                 SC.Profiles.AddRange(profiles);
+
+                SC.SaveChanges();
+                
+            if (!SC.Userdata.Any())
+                SC.Userdata.AddRange(Userdatas);
+
+
 
             await SC.SaveChangesAsync();
         }
@@ -58,6 +61,15 @@ new Profile(){Email = "donotrespond@pof.com", Password = "C%knyNXy6&$A&fbP"},
 new Profile(){Email = "userservices@eharmony.com", Password = "K*c4P@URhxye&fbP"},
 new Profile(){Email = "ghemn.kataa@dpttso8dag0.cf", Password = "GJ&^F9bDfVEx"},
 new Profile(){Email = "jginspace@outlook.com", Password = "RHJ$SaB52&ds"}
+        };
+        #endregion
+        #region UserData
+        List<Userdata> Userdatas = new List<Userdata>(){
+            new Userdata(){IdProfile = 1, Login = "admiral", Name="John",LastName="Minsky"}//, Location="Ezreuford",Age=25,Gender=0},
+        //    new Userdata(){IdProfile = 2, Login = "TheBigPig", Name="Ursula",LastName="Boyd", Location="Deokreit",Age=19,Gender=1},
+         //   new Userdata(){IdProfile = 3, Login = "Genius", Name="Cornelius",LastName="Marsh", Location="Hongqiao",Age=22,Gender=0},
+          //  new Userdata(){IdProfile = 4, Login = "littleKitty", Name="Peny",LastName="Skinner", Location="Cruzeiro",Age=34,Gender=1},
+          //  new Userdata(){IdProfile = 5, Login = "Destructor", Location="Skopje",Age=23,Gender=2}
         };
         #endregion
     }
