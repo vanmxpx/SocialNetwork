ccfb5153dcc2842f5bb836f1b1419a090f689f7c
stormil <11odinets@gmail.com>
2018-08-06 22:01:26 +0300

    return initializer

--- SocialNetwork/Startup.cs
+++ SocialNetwork/Startup.cs
@@ -39,12 +39,11 @@ namespace SocialNetwork
                         mysqlOptions.ServerVersion(new Version(8, 0, 12), ServerType.MySql); // replace with your Server Version and Type
                     }
             ));
-            // services.AddTransient<Intitializer>();
+            services.AddTransient<Intitializer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        // public void Configure(IApplicationBuilder app, IHostingEnvironment env, Intitializer ini)
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Intitializer ini)
         {
             if (env.IsDevelopment())
             {
