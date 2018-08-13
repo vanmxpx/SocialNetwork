a0820c757cc9a35fb3a8c2d16b81b97dfa2047b5
Irina <cg.irenecooper@gmail.com>
2018-08-13 16:40:10 +0300

    [Added] ProfileDto

---
+++ SocialNetwork/ModelDto/ProfileDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialNetwork
+{
+    public class ProfileDto
+    {
+        public ProfileDto()
+        {
+            Blogers = new HashSet<Followings>();
+            Subscribers = new HashSet<Followings>();
+            Posts = new HashSet<Post>();
+        }
+
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public string Location { get; set; }
+        public byte? Age { get; set; }
+        public byte[] Photo { get; set; }
+
+        public ICollection<Followings> Blogers { get; set; }
+        public ICollection<Followings> Subscribers { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
