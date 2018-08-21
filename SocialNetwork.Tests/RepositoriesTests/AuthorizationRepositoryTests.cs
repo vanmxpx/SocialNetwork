5081488adb852ca792b58553eb625125768015a2
Nikita Lipnyagov <nikitalipnagov@gmail.com>
2018-08-21 12:48:57 +0300

    [Add]AuthorizationRepositoryTests, CredentionalRepositoryTests

---
+++ SocialNetwork.Tests/RepositoriesTests/AuthorizationRepositoryTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork;
+using SocialNetwork.Repositories;
+using SocialNetwork.Tests;
+using Xunit;
+
+namespace SocialNetworkTests
+{
+    public class AuthorizationRepositoryTests
+    {
+        private readonly AuthorizationRepository repository;
+        public AuthorizationRepositoryTests()
+        {
+             //INITIALIZATION
+             this.repository = new AuthorizationRepository(DbContextCreator.GetDbContext());
+        }
+
+        [Theory]
+        [InlineData(1, "2019-09-09 09:30:44")]
+        [InlineData(11, "2019-09-05 21:06:38")]
+        [InlineData(21, "2019-09-26 04:32:53")]
+        public void GetByIdTest_Authorization_Expected(int id, string dateTime)
+        {   
+            //WHEN
+            Authorization authorization = repository.GetById(id).Result;
+            //THEN
+            Assert.Equal(dateTime, authorization.DatetimeRequest.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+
+        [Fact]
+        public async void GetAllTest()
+        {   
+            //WHEN
+            ICollection<Authorization> list = await repository.GetAll().ToListAsync();
+            //THEN
+            Assert.Equal(100, list.Count);
+        }
+
+        [Fact]
+        public async void UpdateAuthorizationTest()
+        {   
+            //WHEN
+            Authorization authorization = await repository.GetById(50);
+            DateTime dateTime = new DateTime();
+            authorization.DatetimeRequest = dateTime;
+            await repository.Update(50, authorization);
+            authorization = await repository.GetById(50);
+            //THEN
+            Assert.Equal(dateTime, authorization.DatetimeRequest);
+        }
+
+        [Fact]
+        public async void CreateAuthorizationTest()
+        {   
+            //WHEN
+            await repository.Create(new Authorization{Id = 101, SystemStatus = "1", CredentialRef = 5, DatetimeStart = new DateTime(), DatetimeRequest = new DateTime()});
+            Authorization authorization = await repository.GetById(101);
+            //THEN
+            Assert.Equal(5, authorization.CredentialRef);
+        }
+
+    }
+}
--- SocialNetwork.Tests/RepositoriesTests/CredentionalRepositoryTests.cs
+++ SocialNetwork.Tests/RepositoriesTests/CredentionalRepositoryTests.cs
@@ -1,7 +1,78 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork;
+using SocialNetwork.Repositories;
+using SocialNetwork.Tests;
+using Xunit;
+
 namespace SocialNetworkTests
 {
     public class CredentionalRepositoryTests
     {
-        
+        private readonly CredentialRepository repository;
+
+        public CredentionalRepositoryTests()
+        {
+             //INITIALIZATION
+             this.repository = new CredentialRepository(DbContextCreator.GetDbContext());
+        }
+
+        [Theory]
+        [InlineData(1, "rutrum.justo.Praesent@nec.net")]
+        [InlineData(11, "elit@egetmollislectus.com")]
+        [InlineData(21, "placerat.orci@hendreritDonecporttitor.edu")]
+         public void GetByEmailTest(int id, string email)
+         {
+             //WHEN
+             Credential credential = repository.GetByEmail(email).Result;
+             //THEN
+             Assert.Equal(id, credential.Id);
+         }
+
+        [Theory]
+        [InlineData(1, "rutrum.justo.Praesent@nec.net")]
+        [InlineData(11, "elit@egetmollislectus.com")]
+        [InlineData(21, "placerat.orci@hendreritDonecporttitor.edu")]
+         public void GetByIdTest(int id, string email)
+         {
+             //WHEN
+             Credential credential = repository.GetById(id).Result;
+             //THEN
+             Assert.Equal(email, credential.Email);
+         }
+
+        [Theory]
+        [InlineData(true, "rutrum.justo.Praesent@nec.net")]
+        [InlineData(true, "elit@egetmollislectus.com")]
+        [InlineData(false, "test@gmail.com")]
+         public void DoesExistEmailTest(bool flag, string email)
+         {
+             //WHEN
+             bool doesExist = repository.IsExist(email).Result;
+             //THEN
+             Assert.Equal(flag, doesExist);
+         }
+
+        [Fact]
+        public async void GetAllTest()
+        {   
+            //WHEN
+            ICollection<Credential> list = await repository.GetAll().ToListAsync();
+            //THEN
+            Assert.Equal(50, list.Count);
+        }
+
+        [Fact]
+        public async void UpdateCredentionalTest()
+        {   
+            //WHEN
+            Credential credential = await repository.GetById(50);
+            credential.Password = "QWERTY1234";
+            await repository.Update(50, credential);
+            credential = await repository.GetById(50);
+            //THEN
+            Assert.Equal("QWERTY1234", credential.Password);
+        }
     }
 }
