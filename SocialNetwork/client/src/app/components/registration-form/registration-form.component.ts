252d371b907af4450c7b955b6a6cede6919aa4b4
Misha Krygin <Noderoid64@gmail.com>
2018-09-24 22:23:47 +0300

    [Update] add validators to registration form

--- SocialNetwork/client/src/app/components/registration-form/registration-form.component.ts
+++ SocialNetwork/client/src/app/components/registration-form/registration-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
-import {RegistrationService} from '../../services/registration/registration.service';
-// import { InputDataValidatorService } from '../../validators/input-data-validator.service';
+import { RegistrationService } from '../../services/registration/registration.service';
 
 
 
@@ -12,39 +11,16 @@ import {RegistrationService} from '../../services/registration/registration.serv
 })
 export class RegistrationComponent implements OnInit {
   public user = new User();
-  // private loginValidator = this.validatorService.getLoginValidator();
-  // private emailValidator = this.validatorService.getEmailValidator();
-  // private passwordValidator = this.validatorService.getPasswordValidator();
-
-  constructor(private registrationService: RegistrationService) { }
+  constructor(private registrationService: RegistrationService) {}
 
   ngOnInit() {
   }
 
   onSubmit() {
-    if (this.user.email === undefined) {
-      // обработка пустого email
-    } else
-      if (this.user.login === undefined) {
-        // обработка пустого login
-      } else
-        if (this.user.password === undefined) {
-          // обработка пустого password
-        } else {
+
           this.registrationService.sendEmail(this.user).subscribe((response: string) => alert(response));
-        }
   }
 
-  // getEmailErrorMessage() {
-  //   return this.validatorService.getEmailErrorMessage();
-  // }
 
-  // getLoginErrorMessage() {
-  //   return this.validatorService.getLoginErrorMessage();
-  // }
-
-  // getPasswordErrorMessage() {
-  //   return this.validatorService.getPasswordErrorMessage();
-  // }
 
 }
